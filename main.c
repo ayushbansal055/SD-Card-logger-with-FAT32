9af99778a6407341cddf5aff321726b97bd696e0
Prabhanshu Kumar Jha <220020045@iitdh.ac.in>
2025-11-27 19:10:36 +0530

    Update main.c

--- main.c
+++ main.c
@@ -7,22 +7,12 @@
 #include "ff.h"
 #include "diskio.h"
 
-// ---------------- LED functions ----------------
 
-//*****************************************************************************
 // FatFs mandatory RTC function. Returns current time in FAT format.
 // This example returns a fixed time: 1st Jan 2025, 0:00:00.
-//*****************************************************************************
+
 DWORD get_fattime (void)
 {
-    /* Packed time format:
-     * Bit 31:25 - Year offset from 1980 (0..127)
-     * Bit 24:21 - Month (1..12)
-     * Bit 20:16 - Day (1..31)
-     * Bit 15:11 - Hour (0..23)
-     * Bit 10:5  - Minute (0..59)
-     * Bit 4:0   - Second / 2 (0..29)
-     */
     return ((DWORD)(2025 - 1980) << 25) // Year = 2025 (45)
           | ((DWORD)1 << 21)           // Month = 1 (January)
           | ((DWORD)1 << 16)           // Day = 1
@@ -30,9 +20,6 @@ DWORD get_fattime (void)
           | ((DWORD)0 << 5)            // Minute = 0
           | ((DWORD)0 >> 1);           // Second = 0
 }
-
-// ---------------- LED functions ----------------
-// ... rest of your main.c code ...
 
 
 void LED_Init(void)
@@ -89,6 +76,8 @@ int main(void)
         }
 
         // Hardware is Good!
+
+    
         LED(0,0,1); // BLUE: Hardware OK
         Delay_ms(500);
 
@@ -101,7 +90,7 @@ int main(void)
     LED(0,1,0);   // GREEN: mounted
     Delay_ms(500);
 
-    // --- Stage 2: Create file ---
+    // --- Stage 2: Delete file ---
 
     if(f_unlink("test.txt") == FR_OK){
         while(1){
